c35c33de2b92813f2a0c37c7d600b9ea5da19bb3
Cory Langille <lang1729@gmail.com>
2016-08-02 12:18:35 -0700

    fix comments, general housekeeping

--- R/pStepwise.R
+++ R/pStepwise.R
@@ -65,7 +65,7 @@ stepfwd <- function(fitCurrent, fullmodel, aEnter = 0.1, forcedOut = NULL) {
   predsNotInModel <- setdiff(predsFull, predsInModel)                                           #list of predictors not in current model, not including forced out predictors
   pvals <- sapply(predsNotInModel, function(x) as.numeric(extractp(x, fMaker(x, fitCurrent))))  #takes each predictor not in the current model, creates a new lm which includes it, and stores its respective p-value. Really ugly, but the only way I could make it work.
   pvals <- unlist(pvals)
-  toAdd <- pvals[which(pvals==min(pvals))]                                             
+  toAdd <- pvals[which(pvals==min(pvals))]                                                      #possible new predictor
   if(length(toAdd)==0) return(fitCurrent)                                                       #returns original model if no new predictors are added
   if(toAdd <= aEnter) return(fMaker(names(toAdd), fitCurrent))                                  #updates and returns new model with additional predictor
   return(fitCurrent)
@@ -87,10 +87,10 @@ stepfwd <- function(fitCurrent, fullmodel, aEnter = 0.1, forcedOut = NULL) {
 stepbwd <- function(fitCurrent, fullmodel, aRemove = 0.1, forcedIn = NULL) {
   predsIncluded <- rownames(anova(fitCurrent))                                               #predictors in current model
   predsIncluded <- predsIncluded[(predsIncluded != "Residuals")]                             #removes "residuals" from predictors
-  predsIncluded <- setdiff(predsIncluded, intersect(predsIncluded, forcedIn))
+  predsIncluded <- setdiff(predsIncluded, intersect(predsIncluded, forcedIn))                #makes sure no forced in predictors get removed
   pvalues <- sapply(predsIncluded, function(x) as.numeric(extractp(x, fitCurrent)))          #checks the p-value for each predictor in current model
   pvalues <- unlist(pvalues)
-  if(length(pvalues)==0) return(fitCurrent) 
+  if(length(pvalues)==0) return(fitCurrent)                                                  #returns current model if there are no more possible predictors to add
   toRemove <- pvalues[which(pvalues == max(pvalues))]                                        #selects the predictor with maximal p-value
   if(length(toRemove)==0) return(fitCurrent)  
   if(toRemove > aRemove) return(fMaker(names(toRemove), fitCurrent, add=F))                  #returns an updated model if the p-value is above the threshold
@@ -110,17 +110,16 @@ stepbwd <- function(fitCurrent, fullmodel, aRemove = 0.1, forcedIn = NULL) {
 #' @export
 #'
 pStepwise <- function(response, fullmodel, aEnter = 0.1, aRemove = 0.1, forcedIn = NULL, forcedOut = NULL) {
-  continue <- TRUE
-  fitBwd <- lm(as.formula(paste(response, "~1")))           #creates an empty model to begin with
-  for(pred in forcedIn) fitBwd <- fMaker(pred, fitBwd)      #adds in forced predictors
-  for(pred in forcedOut) fullmodel <- fMaker(pred, fullmodel, add=F)
-  while(continue){
-    fitFwd = stepfwd(fitBwd, fullmodel, forcedOut = forcedOut)                     #try to add a predictor
-    if(identical(fitFwd, fitBwd) == T) {                    #if no new predictors were added, it will stop
+  fitBwd <- lm(as.formula(paste(response, "~1")))                                  #creates an empty model to begin with (poor name choice but it makes the while loop easier)
+  for(pred in forcedIn) fitBwd <- fMaker(pred, fitBwd)                             #adds in forced predictors to initial model
+  for(pred in forcedOut) fullmodel <- fMaker(pred, fullmodel, add=F)               #removes forced out predictors from fullmodel
+  while(TRUE){
+    fitFwd = stepfwd(fitBwd, fullmodel, forcedOut = forcedOut)                     #function that tries to add a predictor to current model
+    if(identical(fitFwd, fitBwd) == T) {                                           #if no new predictors were added, it will stop
       cat("Predictors forced in: ", forcedIn, "\n")
       cat("Predictors forced out: ", forcedOut)
       return(fitFwd)
-    }else {                                                 #try to remove a predictor
+    }else {                                                                        #function that tries to remove a predictor from current model
       fitBwd = stepbwd(fitFwd, fullmodel, forcedIn = forcedIn)
     }
   }
